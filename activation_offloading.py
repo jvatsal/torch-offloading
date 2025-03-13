Asynchronous Activation Offloading Fix + Optimizer/Gradient Offloading [WIP]

activation_offloading.py
@@ -24,9 +24,14 @@ class OffloadActivations(saved_tensors_hooks):
     self.id += 1
     currId = self.id
     size = activation.nelement() * activation.element_size()
-    print(f"pack_tensor: id={currId}, size={size} bytes, device={activation.device}")
 
-    if size >= self.min_offload_size and activation.is_cuda:
+    if (
+      size >= self.min_offload_size 
+      and activation.is_cuda 
+      and not isinstance(activation, (torch.nn.Parameter, torch.nn.Buffer))
+    ):
+      self.comm_stream.wait_stream(torch.cuda.default_stream())
+      
       with torch.cuda.stream(self.comm_stream):
         cpu_tensor = torch.empty_like(activation, pin_memory=self.use_pin_memory, device="cpu")
         cpu_tensor.copy_(activation, non_blocking=True)
@@ -35,6 +40,9 @@ class OffloadActivations(saved_tensors_hooks):
     else:
       self.offloaded_activations[currId] = OffloadedActivation(activation, False)
 
+    # self.comm_stream.synchronize()
+    # torch.cuda.synchronize()
+    # torch.cuda.default_stream().synchronize()
     return currId
 
   def unpack_tensor(self, activation_id: int) -> torch.Tensor:
@@ -43,11 +51,14 @@ class OffloadActivations(saved_tensors_hooks):
     offloaded_act = self.offloaded_activations.pop(activation_id)
     if offloaded_act.is_offloaded:
       with torch.cuda.stream(self.comm_stream):
+        # offloaded_act.event.synchronize()
         self.comm_stream.wait_event(offloaded_act.event)
         gpu_tensor = offloaded_act.tensor.to("cuda", non_blocking=True)
-      torch.cuda.current_stream().wait_stream(self.comm_stream)
-      print(f"unpack_tensor: id={activation_id} restored from CPU to GPU")
+        del offloaded_act.tensor
+
+      # torch.cuda.default_stream().wait_stream(self.comm_stream)
+      # self.comm_stream.synchronize()
+      # torch.cuda.current_stream().wait_stream(self.comm_stream)
       return gpu_tensor
     else:
-      print(f"unpack_tensor: id={activation_id} no offloading needed")
       return offloaded_act.tensor

tests/test_activation_offloading.py
@@ -1,4 +1,5 @@
 import os
+import copy
 os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":16:8"
 import torch
 import pytest
@@ -40,12 +41,12 @@ def run_model(
   """
   model.zero_grad()
 
-  with OffloadActivations(min_offload_size=1024, use_pin_memory=True) if use_offloading else contextlib.nullcontext():
+  with OffloadActivations() if use_offloading else contextlib.nullcontext():
+    model.gradient_checkpointing_enable({"use_reentrant": False})
+
     outputs = model(**inputs)
     loss = outputs.last_hidden_state.mean()
     loss.backward()
-
-  torch.cuda.synchronize()
 
   grad = {}
   for name, param in model.named_parameters():
@@ -57,12 +58,13 @@ def run_model(
 def test_activation_offloading_accuracy() -> None:
   """Runs the model with and without activation offloading and compares the loss and gradients."""
 
-  torch.manual_seed(2024)
   model_no_offloading, inputs_no_offloading = get_model()
-  torch.manual_seed(2024)
-  model_with_offloading, inputs_with_offloading = get_model()
+  model_with_offloading = copy.deepcopy(model_no_offloading)
+  inputs_with_offloading = copy.deepcopy(inputs_no_offloading)
 
+  torch.manual_seed(2024)
   loss_no_offloading, grad_no_offloading = run_model(model_no_offloading, inputs_no_offloading, use_offloading=False)
+  torch.manual_seed(2024)
   loss_with_offloading, grad_with_offloading = run_model(model_with_offloading, inputs_with_offloading, use_offloading=True)
 
   atol = rtol = 1e-5